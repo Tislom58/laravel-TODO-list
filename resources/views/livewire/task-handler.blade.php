eeb4db15b0201cf75a7f17b86ed1db8af8ef0a7c
danielmuller <danmuller.astro@gmail.com>
2024-05-10 16:16:27 +0200

    task icon buttons

--- resources/views/livewire/task-handler.blade.php
+++ resources/views/livewire/task-handler.blade.php
@@ -1,8 +1,5 @@
 <div class="task-icons">
-    <img src="{{ Vite::asset('resources/images/complete-icon.svg') }}"
-         alt="Complete icon" width="5%" height="5%">
-    <img src="{{ Vite::asset('resources/images/edit-icon.svg') }}"
-         alt="Edit icon" width="5%" height="5%">
-    <img src="{{ Vite::asset('resources/images/delete-icon.svg') }}"
-         alt="Delete icon" width="5%" height="5%">
+    <button id="complete-icon"></button>
+    <button id="edit-icon"></button>
+    <button id="delete-icon"></button>
 </div>
